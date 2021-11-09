3066ed353d5a09f7ae4cb2da757f63723c55c16a
Jake Adler <63482878+jrjradler@users.noreply.github.com>
2021-11-09 16:53:45 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,11 @@ const Index: NextPage = () => (
         <Heading title="Hey There! I'm Jake Adler" />
       </div>
       <p className="text-gray-400 text-lg mb-14">
-        I'm a founder and inventor of cool shit based in Toronto. I'm currently building{' '}
+        I'm a developer and inventor of cool shit based in Toronto. I'm currently working on{' '}
         <a href="https://meetordy.com" target="_blank" className="font-semibold hover:text-gray-300 underline">
-          MeetOrdy
+          BCI and Crypto
         </a>{' '}
-        the first AI-based personalized crypto wallet. I focus all my work on projects that directly address problems people face everyday. On this site
+        related projects. I focus all my work on projects that directly address problems people face everyday. On this site
         you'll find some of my recent projects and other information about myself. Thanks for stopping by!
       </p>
       <div className="mb-14">
